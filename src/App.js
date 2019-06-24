e07357744ebb88391b48fc7d26fa174e986f9f2a
circlezero <jwy0565@gamil.com>
2019-06-24 15:00:23 +0900

    glance study The End

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
+import theme from './theme'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+  }
+`
+
+const Card = styled.div`
+  background-color:white;
+`;
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor}
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Form />
+        </Container>
+      </ThemeProvider>
+    </>
   );
 }
+
+const Form = () => (
+  <Card>
+    <Button>Hello</Button>
+  </Card>
+)
+
+const Container = styled.div`
+  height:100vh;
+  width:100%;
+  background-color: pink;
+`;
 
 export default App;
